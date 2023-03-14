03a75ea5535a29c6e8f779ce88e6256a04e76817
Jed Stewart <jsphoenix01@gmail.com>
2023-03-14 16:08:22 -0700

    - Removed WS

--- src/main/java/com/hfla/service/calendar/controllers/CalendarController.java
+++ src/main/java/com/hfla/service/calendar/controllers/CalendarController.java
@@ -1,24 +1,18 @@
 package com.hfla.service.calendar.controllers;
 
 import com.nylas.RequestFailedException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.hfla.service.calendar.pojos.Cronify.Events;
-
 import com.hfla.service.calendar.services.CronofyService;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.*;
-
 import java.util.ArrayList;
 import java.util.UUID;
-
 import java.io.IOException;
 
 
